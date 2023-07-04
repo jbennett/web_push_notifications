4310a5747cebeae3a2f4590902d322aba51a767a
Jonathan Bennett <jonathan@jbennett.me>
2023-07-04 17:08:46 -0400

    remove expired subscriptions

--- app/notifications/delivery_methods/web_push.rb
+++ app/notifications/delivery_methods/web_push.rb
@@ -2,6 +2,9 @@ class DeliveryMethods::WebPush < Noticed::DeliveryMethods::Base
     def deliver
         recipient.web_push_subscriptions.each do |subscription|
             subscription.publish(notification.data)
+        rescue WebPush::ExpiredSubscription
+            Rails.logger.info "Removing expired WebPush subscription"
+            subscription.destroy
         end
     end
 end
