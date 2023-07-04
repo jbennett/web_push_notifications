236b5341cb1df441e91746101c0754fcf8428470
Jonathan Bennett <jonathan@jbennett.me>
2023-07-04 16:41:25 -0400

    request permissions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,36 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener("turbo:load", () => {
+    switch (Notification.permission) {
+        case "granted":
+            // send to server
+            return
+        case "denied":
+            // do nothing?
+            return
+        default:
+            promptForNotifications()
+    }
+})
+
+function promptForNotifications() {
+    const notificationsButton = document.getElementById("enable_notifications")
+    if (!notificationsButton) return
+
+    notificationsButton.classList.remove("hidden")
+    notificationsButton.addEventListener("click", event => {
+        event.preventDefault()
+        Notification.requestPermission()
+            .then((permission) => {
+                if (permission === "granted") {
+                    new Notification("Notiifcations enabled") // test notification
+                } else {
+                    alert("Notifications declined")
+                }
+            })
+            .catch(error => console.log("Notifications error", error))
+            .finally(() => notificationsButton.classList.add("hidden"))
+    })
+}
