5f0642ec898f7b94091644033f2609d6971db42a
Jonathan Bennett <jonathan@jbennett.me>
2023-07-04 17:03:19 -0400

    Add service worker script

--- esbuild.config.js
+++ esbuild.config.js
@@ -24,3 +24,26 @@ esbuild.context({
         ctx.dispose()
     }
 }).catch(() => process.exit(1))
+
+
+esbuild.context({
+    entryPoints: ["service_worker.js"],
+    bundle: true,
+    outdir: path.join(process.cwd(), "public"),
+    absWorkingDir: path.join(process.cwd(), "app/javascript"),
+    sourcemap: true,
+    minify: false,
+    loader: {
+        '.js': 'jsx',
+        '.png': 'file',
+        '.svg': 'file',
+        '.jpg': 'file',
+    },
+}).then((ctx) => {
+    if (watching) {
+        ctx.watch()
+    } else {
+        ctx.rebuild()
+        ctx.dispose()
+    }
+}).catch(() => process.exit(1))
