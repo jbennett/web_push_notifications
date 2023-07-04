d7e92a64903f32c193d7399f61224aecd316ce78
Jonathan Bennett <jonathan@jbennett.me>
2023-07-04 16:33:02 -0400

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.find_or_create_by(email: "admin@example.com") { _1.password = "password" }
+User.find_or_create_by(email: "user1@example.com") { _1.password = "password" }
