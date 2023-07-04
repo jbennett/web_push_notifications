c02ae717304ab85b4ea40afba8dd54fc1b5ef6d2
Jonathan Bennett <jonathan@jbennett.me>
2023-07-04 17:07:18 -0400

    handle inbound pushes

--- app/javascript/service_worker.js
+++ app/javascript/service_worker.js
@@ -1,1 +1,21 @@
-console.log("Service worker 2")
+const VERSION = "Accept Push 1"
+
+self.addEventListener("install", event => {
+    console.log(`Installing ${VERSION}`)
+})
+
+self.addEventListener("activate", event => {
+    console.log(`Activating ${VERSION}`)
+})
+
+self.addEventListener('push', event => {
+    const data = event.data?.json() || {}
+    console.log("Received push", data)
+    
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            data: data,
+        })
+    )
+})
